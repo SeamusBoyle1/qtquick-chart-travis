235d34dac7e53864f130bf84d0c371d7f3fad9e5
Seamus Boyle <s3292445@student.rmit.edu.au>
2017-09-11 13:34:20 +1000

    Add boost-1.65.1 dep

--- src/myapp/main.cpp
+++ src/myapp/main.cpp
@@ -1,6 +1,10 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 
+#include <QDebug>
+
+#include <boost/optional.hpp>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -10,6 +14,14 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+
+    boost::optional<int> amIAnInt;
+    {
+        amIAnInt = boost::make_optional(true, 111);
+    }
+    if (amIAnInt) {
+        qDebug() << "Yes, I'm" << *amIAnInt;
+    }
 
     return app.exec();
 }
