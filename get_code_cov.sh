11ba7e9db5f263c1a5113025c74c1f476d4303fd
Seamus Boyle <s3292445@student.rmit.edu.au>
2017-09-12 12:35:32 +1000

    Fix gcov and shadow build

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for filename in `find . | egrep '\.cpp'`;
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SCRIPT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+GCOV="gcov-4.9"
+
+SOURCE_DIR="../qtquick-chart-travis"
+
+for filename in `find $SOURCE_DIR | egrep '\.cpp'`;
 do
-  gcov-4.9 -n -o . $filename > /dev/null;
+    out_dirname=$(echo $filename | cut -d"/" -f3-)
+    $GCOV -n -o $out_dirname $filename > /dev/null;
 done
