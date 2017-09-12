5d0fc158acd1b32116142f33300b33a62354adf6
Seamus Boyle <s3292445@student.rmit.edu.au>
2017-09-12 13:08:23 +1000

    Don't hardcode repo name

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -5,7 +5,8 @@ SCRIPT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GCOV="gcov-4.9"
 
-SOURCE_DIR="../qtquick-chart-travis"
+# Assumes build dir and source dir are siblings
+SOURCE_DIR="../$(echo $(basename $SCRIPT_DIR_PATH))"
 
 for filename in `find $SOURCE_DIR -not -path "$SOURCE_DIR/boost*" -not -path "$SOURCE_DIR/Qt/*" | egrep '\.cpp'`;
 do
