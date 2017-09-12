9d613e7c0209f7d08c4f07a2d19f9a1ad3601bac
Seamus Boyle <s3292445@student.rmit.edu.au>
2017-09-12 16:28:39 +1000

    Don't shadow build on travis

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-SCRIPT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-GCOV="gcov-4.9"
-
-# Assumes build dir and source dir are siblings
-SOURCE_DIR="../$(echo $(basename $SCRIPT_DIR_PATH))"
-
-for filename in `find $SOURCE_DIR -not -path "$SOURCE_DIR/boost*" -not -path "$SOURCE_DIR/Qt/*" | egrep '\.cpp'`;
+for filename in `find . | egrep '\.cpp'`;
 do
-    out_dirname=$(echo $filename | cut -d"/" -f3-)
-    $GCOV -n -o $out_dirname $filename > /dev/null;
+  gcov-4.9 -n -o . $filename > /dev/null;
 done
